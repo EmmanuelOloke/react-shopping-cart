bdcfb72e7dadb73d9eaea15dba1b7f693d1f8b0a
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-01-28 22:23:35 +0100

    created Product.js and formatted how products get displayed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
 import Cart from './components/Cart';
 import Header from './components/Header';
 import Main from './components/Main';
+import data from './data';
 
 function App() {
+  const {products} = data;
     return ( 
       <div className = "App" >
         <Header/>
         <div className="row">
-          <Main/>
-          <Cart/>
+          <Main products={products}></Main>
+          <Cart></Cart>
         </div>
         
       </div>
