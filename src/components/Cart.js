3c96c81673b2a3d10d30a2d7c439768460823be5
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-01-28 21:53:13 +0100

    created data.js to hold sample products

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Cart(props) {
-    return <div> Cart </div>
+    return (
+        <aside className="block col-1">
+            <h2>Cart Items</h2>
+        </aside>
+    );
 }
